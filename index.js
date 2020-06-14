61413a1fb8d73c15560272e33c0503e160525c93
Carla Holcomb <carla.programmer@gmail.com>
2020-06-14 02:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,38 @@
 function saturdayFun(saying="roller-skate") {
   return(`This Saturday, I want to ${saying}!`);
 }
+
+let mondayWork = function(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(beg="*") {
+  return function(adjective="special") {
+    return `You are ${beg}${adjective}${beg}!`
+  }
+}
+
+const Calculator = {
+  add: function(a,b) {
+    return a + b;
+  },
+  subtract: function(a,b) {
+    return a - b
+  },
+  multiply: function(a,b) {
+    return a * b
+  },
+  divide: function(a,b) {
+    return a / b;
+  }
+}
+
+let actionApplyer = function(start, ray) {
+  let a = start
+
+  for (let i = 0; i < ray.length; i++ ){
+    a = ray[i](a)
+  }
+
+  return a
+}
